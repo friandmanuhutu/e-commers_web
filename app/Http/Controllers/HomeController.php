eaa3d4a3b25d415804f22c040d310fecc4546f8c
Hammam Karim <hammmamjauharul@gmail.com>
2024-10-14 11:42:58 +0700

    video 24

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -101,4 +101,21 @@ class HomeController extends Controller
 
         return redirect()->back();
     }
+
+    public function mycart()
+    {
+
+        if(Auth::id())
+        {
+            $user = Auth::user();
+
+            $userid = $user->id;
+
+            $count = Cart::where('user_id',$userid)->count();
+
+            $cart = Cart::where('user_id',$userid)->get();
+        }
+
+        return view('home.mycart',compact('count','cart'));
+    }
 }
